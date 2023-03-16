9b0618a937c47e8cf6bad77dc4fb206e9eac07a2
yeXela13 <alexey13.90@mail.ru>
2023-03-16 21:32:43 +0300

    Fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -95,7 +95,7 @@ class Api {
 
 
 const api = new Api({
-    baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-59/',
+    baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-59',
     headers: {
         authorization: 'fb15d3cd-51f7-4c56-adbf-e4fa5201b028',
         'Content-Type': 'application/json'
